e819103c52ed96681a57e3c6c60ea3325cc7e739
Gacek <szymon.gackowski@gmail.com>
2020-08-16 11:42:57 +0200

    fix

--- resources/views/domains/webinar/webinar.blade.php
+++ resources/views/domains/webinar/webinar.blade.php
@@ -3,13 +3,10 @@
 @section('content')
     <p>Content</p>
     @if($webinar->isActive())
-        <p>Is active</p>
         @guest()
-            <p>Guest</p>
             @include('domains.webinar.subscribe')
         @endguest
         @auth()
-            <p>Auth</p>
             @include('domains.webinar.active')
         @endauth
     @elseif($webinar->isFuture())
