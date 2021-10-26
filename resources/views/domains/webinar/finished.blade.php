85668f0d79da86405ea943f3775f00ebf57b509c
Szymon Gackowski <szymon.gackowski@gmail.com>
2021-10-26 19:45:09 +0200

    Finished background

--- resources/views/domains/webinar/finished.blade.php
+++ resources/views/domains/webinar/finished.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <h3 class="text-white">Ten webinar się zakończył</h3>
+        <div class="col-md-12 bg-white">
+            <h3 class="text-black-50">Ten webinar się zakończył</h3>
         </div>
     </div>
 </div>
